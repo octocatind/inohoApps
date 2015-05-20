c38c3289a7dd4cfb63d4ecf41d3c07525e3335bf
octocatind <octocatind@gmail.com>
2015-05-20 20:09:30 +0530

    Saving a copy of Perf logging changes

--- android/InohoConnect/src/com/inoho/inohoconnect/InohoMainActivity.java
+++ android/InohoConnect/src/com/inoho/inohoconnect/InohoMainActivity.java
@@ -1,6 +1,7 @@
 package com.inoho.inohoconnect;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
@@ -15,6 +16,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.inoho.inohoconnect.InohoConnectionManager.CONNECTION_TYPE;
 
@@ -138,6 +140,8 @@ public class InohoMainActivity extends Activity {
     		public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
 				if(m_retry < 5) {
 					m_retry++;
+					addPerformaceData("error loading trying " + m_retry + " time");
+					
 					Handler h = new Handler();
 					h.postDelayed(new Runnable() {
 						
@@ -154,6 +158,8 @@ public class InohoMainActivity extends Activity {
 			}
 			 
 			public void onPageFinished (WebView view, String url){
+				addPerformaceData("done with loading " + url);
+				
 				RelativeLayout fmflash = (RelativeLayout) findViewById(R.id.flashScreen);
 				fmflash.setVisibility(View.INVISIBLE);
 				 
@@ -161,15 +167,48 @@ public class InohoMainActivity extends Activity {
 				fmWebView.setVisibility(View.VISIBLE);
 				
 				RelativeLayout fmOfflineWebView = (RelativeLayout) findViewById(R.id.offlineScreen_RL);
-				fmOfflineWebView.setVisibility(View.INVISIBLE);
+				fmOfflineWebView.setVisibility(View.INVISIBLE);	
+				
+				showPerformanceData();
 			 }
 		 });
 	}
 	
 	//private functionalities
+	
+	private void showPerformanceData() {
+		if(m_perfDlg == null) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(m_perfData).setTitle("Inoho Performance Data");
+			m_perfDlg = builder.create();
+			ScrollView sv = new ScrollView(this);
+			m_perfDlg.setView(sv);
+		}
+		
+		m_perfDlg.show();
+		
+		m_handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				if(m_perfDlg != null)
+					m_perfDlg.dismiss();				
+			}
+		}, 5000);
+		
+	}
+	
+	private void addPerformaceData(String msg) {
+		m_perfData += "+ " + getTimeLapsedFromStart() + "s " + msg + "\n";
+	}
+	
+	private long getTimeLapsedFromStart() {
+		return (System.currentTimeMillis() - m_startTime)/1000;
+	}
+	
 	private void loadHomePage() {
 		//all set now load content
 		 if(!m_linkToLoad.isEmpty()) {
+			 addPerformaceData("loading " + m_linkToLoad);
 			 mWebView.loadUrl(m_linkToLoad);
 		 } else {
 			 //mWebView.loadData(mOfflineInfo,"text/html", "");
@@ -182,35 +221,51 @@ public class InohoMainActivity extends Activity {
 		new Thread(new Runnable() {			
 			@Override
 			public void run() {
-				String linkToConnect = m_conMgr.getHomeLink(false);			  
+				addPerformaceData("getting home (full)");
+				
+				String linkToConnect = m_conMgr.getHomeLink(false);
+				
+				addPerformaceData("got home @ " + linkToConnect + " (full)");
+				
 				if(!linkToConnect.equalsIgnoreCase(m_linkToLoad)) {
 					m_linkToLoad = linkToConnect;
 					m_handler.postDelayed(new Runnable() {
 						@Override
 						public void run() {
-						  InohoMainActivity.this.loadHomePage();
+							InohoMainActivity.this.loadHomePage();
 						}
 					}, 500);
+				} else {
+					addPerformaceData("same home as quick scan, no re-loading");
 				}
 			}
 		}).start();
 	}
 		
 	private void openHomeLinkInQuickMode() {
+		//reset performance data
+		m_startTime = System.currentTimeMillis();
+		m_perfData = "";
 		//let's try quick connect first and then full
 		m_handler = new Handler();
 		
 		new Thread(new Runnable() {			
 			@Override
 			public void run() {
-				m_linkToLoad = m_conMgr.getHomeLink(true);			  
-				  
+				addPerformaceData("getting home(quick)");
+				m_linkToLoad = m_conMgr.getHomeLink(true);
+				addPerformaceData("got home @ " + m_linkToLoad + " (quick)");  
+				
 				m_handler.postDelayed(new Runnable() {
 					  @Override
 					  public void run() {
 						  InohoMainActivity.this.loadHomePage();
-						  if(m_conMgr.getCurrentConnectionType() != CONNECTION_TYPE.MOBILE)
+						  if(m_conMgr.getCurrentConnectionType() != CONNECTION_TYPE.MOBILE) {
+							  addPerformaceData("starting full scan (quick)");
 							  openHomeLinkInFullMode();
+						  } else {
+							  addPerformaceData("on 3G, no full scan (quick)");
+						  }
 					  }
 					}, 500);
 			}
@@ -235,5 +290,10 @@ public class InohoMainActivity extends Activity {
 	private WebView mWebView;
 	private InohoConnectionChangeListner m_inohoCnctnListner = null;
 	private InohoConnectionManager m_conMgr = null;
-	private Handler m_handler = null;		
+	private Handler m_handler = null;
+	
+	//performance data
+	private long m_startTime = 0;
+	private String m_perfData = "";
+	private AlertDialog m_perfDlg = null;
 }
